6e6daaaac25edcc4f1289359ec3270a2f694c3cb
strait593 <dparhomenko532@gmail.com>
2026-02-28 22:39:46 +0100

    Turned the function into a generator

--- task2.py
+++ task2.py
@@ -1,10 +1,14 @@
 import numbers
 from typing import Callable
+import re
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 
 def generator_numbers(text: str):
-    return [float(numbers) for numbers in text.split() if numbers.replace('.','').isdigit()]
+    pattern = r"\d+\.\d+|\d+"
+    
+    for match in re.finditer(pattern, text):
+        yield float(match.group())
 
 def sum_profit(text: str,func: Callable):
     return sum(func(text))
